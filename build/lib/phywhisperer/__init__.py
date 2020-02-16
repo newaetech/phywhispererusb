e4abf2224b3f6fcb141dd6924c1fdd7502a6545e
CreateRemoteThread <elegura.wolfe@gmail.com>
2020-02-15 22:33:13 -0500

    cleanup

--- build/lib/phywhisperer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Core helper functions and definitions for PhyWhisperer.
-"""
-
-# allow the definition of a USBEventSink to be referenced as though it were in this namespace.
-from .sniffer import USBEventSink
