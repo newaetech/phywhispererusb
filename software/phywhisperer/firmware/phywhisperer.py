a4ccacbb6c999fa35220143f849d5b5c67cb48d0
jpcrypt <jpthibault@newae.com>
2020-09-21 14:52:26 -0400

    FW: bump version number.
    Forgot to do it in previous firmware update commit.

--- software/phywhisperer/firmware/phywhisperer.py
+++ software/phywhisperer/firmware/phywhisperer.py
@@ -1,10 +1,10 @@
 # This file was auto-generated. Do not manually edit or save. What are you doing looking at it? Close it now!
-# Generated on 2020-09-08 13:51:44.782259
+# Generated on 2020-09-21 14:51:45.348554
 #
 import binascii
 import io
 
-fwver = [0, 0]
+fwver = [1, 0]
 def getsome(item, filelike=True):
     data = _contents[item].encode('latin-1')
     data = binascii.a2b_base64(data)
