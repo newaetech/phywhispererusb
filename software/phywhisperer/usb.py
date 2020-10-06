15ebbc86ffbad8fe0fc7309738f226d08bc8d27a
jpcrypt <jpthibault@newae.com>
2020-10-06 12:03:21 -0400

    Add userio module.

--- software/phywhisperer/usb.py
+++ software/phywhisperer/usb.py
@@ -93,7 +93,7 @@ class Usb(PWPacketDispatcher):
                     else:
                         logging.warning("Couldn't parse line: %s", define)
         # make sure everything is cool:
-        assert self.verilog_define_matches == 58, "Trouble parsing Verilog defines file (%s): didn't find the right number of defines." % defines_file
+        assert self.verilog_define_matches == 60, "Trouble parsing Verilog defines file (%s): didn't find the right number of defines." % defines_file
         defines.close()
 
 
